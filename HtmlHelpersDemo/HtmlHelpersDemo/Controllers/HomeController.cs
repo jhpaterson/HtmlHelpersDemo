1d2c7aa188ac4dac6693214ac632b922390efbba
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-17 23:19:11 +0100

    initial commit

---
+++ HtmlHelpersDemo/HtmlHelpersDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HtmlHelpersDemo.Models;
+
+namespace HtmlHelpersDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.ProductName = "Onion";
+
+            Product product = new Product { ProductID = 1, ProductName = "Banana", Description = "Yellow", Price = 9.99M };
+
+            return View(product);
+        }
+
+        public ActionResult Grid()
+        {
+            IEnumerable<Product> products = new List<Product>{
+                new Product{ProductID=1,ProductName="Banana",Description="Yellow",Price=9.99M},
+                new Product{ProductID=2,ProductName="Apple",Description="Green",Price=9.99M},
+                new Product{ProductID=3,ProductName="Orange",Description="Blue",Price=9.99M}
+            };
+
+            return View(products);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ HtmlHelpersDemo/HtmlHelpersDemo/Helpers/LabelExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace HtmlHelpersDemo.Helpers
+{
+    public static class LabelExtensions
+    {
+        public static MvcHtmlString RedLabel(this HtmlHelper helper, string target, string text)
+        {
+            return new MvcHtmlString(
+                String.Format("<label for='{0}' style='color:red'>{1}</label>", target, text));
+
+        }
+    }
+}
